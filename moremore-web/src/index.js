d6550e33c5bfcf04b6116484bff7252e8b4a9918
CuteCoder <wave_onlyone@hotmail.com>
2018-07-04 19:11:20 +0700

    edit path for boostrap

--- moremore-web/src/index.js
+++ moremore-web/src/index.js
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-import React ,{Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
