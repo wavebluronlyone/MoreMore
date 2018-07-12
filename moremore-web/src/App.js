e3d14cf1646f9d1742dd99cec48c662d6bd335ff
wave_onlyone@hotmail.com <wave_onlyone@hotmail.com>
2018-07-12 21:25:56 +0700

    remove vscode configuration

--- moremore-web/src/App.js
+++ moremore-web/src/App.js
@@ -1,25 +1,14 @@
-import React,{Component} from 'react';
+import React from 'react';
 import Routes from './routes';
-import Home from './Pages/Home'
 import { Header, Footer } from './Layout';
 import './Styles/App.css';
 
-// const App = () => (
-//   <div>
-//     <Header />
-//       <Routes exact path="/" component={Home}/>
-//     <Footer />
-//   </div>
-// );
-class App extends Component{
-  render(){
-    return(
-      <div>
-        <Header />
-        <Routes />
-        <Footer />
-      </div>
-    )
-  }
-}
-export default App;
+export default () => (
+  <React.Fragment>
+    <Header />
+    <Routes />
+    <Footer />
+  </React.Fragment>
+);
+
+// export default Rotes;
