c9a20b9cfdff777849122c815b20f29190b9fffc
EarthCSTU <pusinza@gmail.com>
2018-08-22 11:18:43 +0700

    footer
    footer

--- moremore-web/src/Layout/Footer.jsx
+++ moremore-web/src/Layout/Footer.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import '../Styles/footer.css';
 
 
 const Footer = () => (
-  <footer>
-    <hr />
-    <p>
-       Copylight 2018
-    </p>
+  <footer class="main">
+    <div class="line">
+
+    </div>
+    <div class="block">
+      <div>
+        picture
+      </div>
+      <div>
+        <p>
+          Care share inspire
+        </p>
+      </div>
+    </div>
+    <div className="block">
+      <p className="foorer-text">xxxxxxxxxxxxx</p>
+      <p className="foorer-text">xxxxxxxxxxxxx</p>
+      <p className="foorer-text">xxxxxxxxxxxxx</p>
+    </div>
+    <div className="block">
+      <p className="foorer-text">xxxxxxxxxxxxx</p>
+    </div>
+    <div className="block block-logo">
+      <p className="text-right">logo facebook</p>
+      <p className="text-right">logo line</p>
+      <p className="text-right">content</p>
+    </div>
   </footer>
 );
 
